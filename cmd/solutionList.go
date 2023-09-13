69c63c6827a72d11e72084abaa11e02305bd372f
Shaun Mitchell <shaunmitchell@google.com>
2023-09-13 20:06:40 +0000

    Initial Import

---
+++ cmd/solutionList.go
@@ -0,0 +1,51 @@
+// Copyright 2023 Shaun Mitchell
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"arete/internal/cmdsolution"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+// solutionCmd represents the create command
+var solutionListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List Solutions",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		whiteBold := color.New(color.FgWhite).Add(color.Bold).SprintFunc()
+
+		sl := cmdsolution.SolutionsList{}
+
+		err := sl.GetCacheSolutions()
+
+		if err != nil {
+			return err
+		}
+		 for _, sl := range sl.Solutions {
+		 	fmt.Printf("%s \n  %s\n", whiteBold(sl.Solution), sl.Description)
+		 }
+
+		 return nil
+	},
+}
+
+// init the command and add flags
+func init() {
+	solutionCmd.AddCommand(solutionListCmd)
+}
---
+++ pkg/config/config.go
@@ -0,0 +1,71 @@
+// Copyright 2023 Shaun Mitchell
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package config
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"os"
+	"strings"
+	"path/filepath"
+
+	"arete/internal/cmdsolution"
+)
+
+// init the config directory and file
+func Init() {
+	usrH, _ := os.UserHomeDir();
+
+	configPath := filepath.Join(usrH, ".arete")
+	configFile := "config.yaml"
+	solutionFile := "solutions.yaml"
+	repoURL := "https://github.com/shaunmitchellve/arete"
+	repoBranch := "main"
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	viper.SetConfigName(configFile[:strings.Index(configFile, ".")])
+	viper.SetConfigType(configFile[strings.Index(configFile, ".")+1:])
+	viper.AddConfigPath(configPath)
+	viper.SetDefault("cache", configPath)
+	viper.SetDefault("repoUrl", repoURL)
+	viper.SetDefault("repoBranch", repoBranch)
+
+	if _, cpErr := os.Stat(configPath); os.IsNotExist(cpErr) {
+		if mkErr := os.Mkdir(configPath, 0744); mkErr != nil {
+			log.Fatal().Err(mkErr).Msg("")
+		}
+	}
+
+	if _, fErr := os.Stat(filepath.Join(configPath, configFile)); os.IsNotExist(fErr) {
+		if err := viper.SafeWriteConfig(); err != nil {
+			log.Fatal().Err(err).Msg("Unable to create config file")
+		}
+	}
+
+	solutionFilePath := filepath.Join(configPath, solutionFile)
+	if _, fErr := os.Stat(solutionFilePath); os.IsNotExist(fErr) {
+		if _, err := os.Create(solutionFilePath); err != nil {
+			log.Fatal().Err(err).Msg("Unable to create cached solution file")
+		}
+	}
+
+	sl := cmdsolution.SolutionsList{}
+	sl.GetCoreSolutions()
+
+	if cErr := viper.ReadInConfig(); cErr != nil {
+		log.Error().Err(cErr).Msg("Unable to read config file")
+	}
+}
